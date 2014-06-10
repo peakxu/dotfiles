533a93b301bda2b2373ff78bfa090a45a0377f16
Peak Xu <peak.xu@gmail.com>
2014-06-09 21:48:38 -0400

    Add Python setup script

---
+++ setup_python.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Install pip
+sudo easy_install pip
+
+# Install virtualenv + wrapper
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+
+# Install IPython globally
+sudo pip install ipython

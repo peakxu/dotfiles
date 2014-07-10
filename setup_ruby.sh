5322ca31f9c70d6c2dad685d032cfd0abb9ef042
Peak Xu <peak.xu@gmail.com>
2014-07-10 07:07:21 -0400

    Use global gemset

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -6,5 +6,16 @@ sudo -v
 # Install RVM stable
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# source RVM profile
+# Reload RVM
+source ~/.bash_profile
+
+# Install 2.1
 rvm install 2.1
+rvm use 2.1 --default
+
+# Create global gemset
+rvm gemset create global
+rvm gemset use global
+
+# Install system-wide gems
+gem install pry

eb08f2081ace10f04534b46ea62152f4ea4ff47c
Peak Xu <peak.xu@gmail.com>
2014-07-09 20:17:21 -0400

    Add sublime text setup script

---
+++ setup_sublime.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+local_sublime="$HOME/Library/Application Support/Sublime Text 2"
+synced_sublime="$HOME/Dropbox/Sublime Text 2"
+
+if [ ! -L "$local_sublime" ]; then
+    rm -rf "$local_sublime"
+    ln -s "$synced_sublime" "$local_sublime"
+fi

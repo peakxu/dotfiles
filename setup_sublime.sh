10f1690a67254e48c3811bf93ed9392a1fd8dcbe
Peak Xu <peak.xu@gmail.com>
2014-07-26 10:37:15 -0400

    Move Sublime Packages into Personalization on DropBox

--- setup_sublime.sh
+++ setup_sublime.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 local_sublime="$HOME/Library/Application Support/Sublime Text 2"
-synced_sublime="$HOME/Dropbox/Sublime Text 2"
+synced_sublime="$HOME/Dropbox/Personalization/Sublime Text 2"
 
 if [ ! -L "$local_sublime" ]; then
     rm -rf "$local_sublime"
